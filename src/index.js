ca4286577d594c16553116815c955557a5540697
Tomas <tomibudeguer20@gmail.com>
2022-10-20 17:17:43 -0300

    configuramos un puerto para nuestro backend e instalamos nodemon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import express from 'express'
-console.log('hola mundo')
+import express from "express";
+
+// crear una instancia de express
+const app = express();
+
+// creamos una variable
+app.set("port", process.env.PORT || 4000);
+
+// usar el puerto
+app.listen(app.get('port'),()=>{
+    console.log('estoy en el puerto '+ app.get('port'))
+})
+
+console.log("hola mundo");
