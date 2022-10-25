cbe2c88b92d22f411dae10103d6020c3249e1ffb
Tomas <tomibudeguer20@gmail.com>
2022-10-25 18:23:22 -0300

    creamos un producto con peticion POST

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,11 +1,27 @@
+import Producto from "../models/producto";
+
 export const listarProductos = (req, res) => {
   res.send("aqui tengo que retornar un arreglo de productos");
 };
 
-export const crearProducto = (req, res) => {
+export const crearProducto = async (req, res) => {
+  try {
     // extraer del body los datos
-    console.log(req.body)
+    console.log(req.body);
     // agregar la validacion correspondiente
+
+    // creamos un nuevo producto
+    const productoNuevo = new Producto(req.body);
     // guardar ese producto en la BD
-  res.send("esto es una prueba de peticion post");
+    await productoNuevo.save();
+    // responder al usuario que todo salio bien
+    res.status(201).json({
+        mensaje: "El producto fue correctamente creado"
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+        mensaje: "Error al intentar agregar un producto"
+    })
+  }
 };
