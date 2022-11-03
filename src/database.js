45c7ffe4b908cd9408db4d08c9b8bd74265c3333
Tomas <tomibudeguer20@gmail.com>
2022-11-03 16:51:04 -0300

    cambie la url

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 // localhost: 127.0.0.1
-const url = 'mongodb://localhost:27017/cafe-branch'
+// const url = 'mongodb://localhost:27017/cafe-branch' //BD-LOCAL
+const url = 'mongodb+srv://TomasBudeguer:KJI5kQLnLz619pLl@cluster0.vjacqdu.mongodb.net/cafe-branch'
 
 mongoose.connect(url)
 
